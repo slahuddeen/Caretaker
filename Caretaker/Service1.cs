9e2643017b7963d39b741c4b8093fce0c1b0a754
Deeny <slahuddeen@googlemail.com>
2020-06-26 17:23:12 +0100

    commit with code
    for anyone who wants to tweak the script.

---
+++ Caretaker/Service1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+
+using System.Timers;
+
+namespace CaretakerV1
+{
+
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Started at : " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 36000;
+            timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+            WriteToFile("Stopped at : " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("resuming at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            //log stuff, made where you droped the service
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\CaretakerLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+                using (StreamWriter sw = File.CreateText(filepath))
+                    sw.WriteLine(Message);
+            else
+                using (StreamWriter sw = File.AppendText(filepath))
+                    sw.WriteLine(Message);
+
+            //logic
+            string[] sSelectedRootPaths = ConfigurationManager.AppSettings["DeleteDirPaths"].Split(',');
+            foreach (string sFolder in sSelectedRootPaths)
+                FindFolders(sFolder, filepath);
+
+        }
+
+
+        protected void FindFolders(string sFolder, string filepath)
+        {
+            //gets the date from the config
+            DateTime date = DateTime.Now.Subtract(TimeSpan.FromDays(Convert.ToInt32(ConfigurationManager.AppSettings.Get("DaysOld"))));
+            var aFolders = Directory.GetDirectories(sFolder);
+            var aFiles = Directory.GetFiles(sFolder);
+
+            //recursive untill it gets allt the folders and files that fit the criteria
+            foreach (string sFile in aFiles)
+            {
+                DateTime dFileCreationDate = System.IO.File.GetCreationTime(sFile);
+
+                if (dFileCreationDate <= date)
+                {
+                    using (StreamWriter sw = File.AppendText(filepath))
+                        sw.WriteLine("found an outdated File, Deleting: " + sFile.ToString());
+
+                    System.IO.File.Delete(sFile);
+                }
+            }    
+
+            foreach (var subFolder in aFolders.ToList())
+                FindFolders(subFolder, filepath);
+        }
+    }
+
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+This commit contains the whole project. for those who want to improve on the script feel free however if you just want to run the service open cmd and run this:
+InstallUtil.exe "The place you put caretaker " + Caretaker\bin\Debug\CaretakerV1.exe
+Then go to services, find Caretaker.V1 and hit start.
